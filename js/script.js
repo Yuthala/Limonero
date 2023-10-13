6c925409ffd1d9aea4ac8f20084438c77ba9ecf2
Dina Polousova <dinapolousova@gmail.com>
2023-10-13 14:56:01 +0300

    XHR

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-//TO DO:
-//2. Пропала кнопка scroll up на мобильной версии
+
 
 
 window.addEventListener('DOMContentLoaded', function() { //загрузка кода после того как загружены элементы DOM-дерева
@@ -148,4 +147,51 @@ window.addEventListener('DOMContentLoaded', function() { //загрузка ко
 			}
 		}
     
+		//Form
+
+		let message = {
+			loading: 'Загрузка...',
+			success: 'Спасибо! Скоро мы с вами свяжемся.',
+			failure: 'что-то пошло не так...'
+		};
+
+		let form = document.querySelector('.contacts__form'),
+			input = form.getElementsByTagName('input'),
+			statusMessage = document.createElement('div');
+
+			statusMessage.classList.add('status');
+
+			form.addEventListener('submit', function(event) {
+				event.preventDefault();
+				form.appendChild(statusMessage);
+
+				let request = new XMLHttpRequest();
+				request.open('POST', 'server.php');
+				request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+				request.responseType = "text";
+
+				let formData = new FormData(form);
+
+				let obj = {};
+				formData.forEach(function(value, key) {
+					obj[key] = value;
+				});
+				let json = JSON.stringify(obj);
+
+				request.send(json);
+
+				request.addEventListener('readystatechange', function() {
+					if (request.readyState < 4) {
+						statusMessage.innerHTML = message.loading;
+					} else if (request.readyState === 4 && request.status == 200) {
+						statusMessage.innerHTML = message.success;
+					} else {
+						statusMessage.innerHTML = message.failure;
+					}
+				});
+
+					for (let i = 0; i < input.length; i++) {
+						input[i].value ='';
+					}
+			});
 });
