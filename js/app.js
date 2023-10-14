493845ae5382bf2ca02c203e5be0efbc958d1153
Dina Polousova <dinapolousova@gmail.com>
2023-10-15 00:51:46 +0300

    async form submit

--- js/app.js
+++ js/app.js
@@ -34,16 +34,54 @@ formArr.forEach((el) => {
 	}
   }
 
-  function buttonHandler(e) {
-	const allValid = [];
+  // Здесь проверяем, можно ли отправить форму
+function formCheck(e) {
+	e.preventDefault(); // блокируем input
+	const allValid = []; // создаем массив валидных значений
 	validFormArr.forEach((el) => {
-	  allValid.push(el.getAttribute("is-valid"));
+	  allValid.push(el.getAttribute("is-valid")); // проверяем каждое поле
 	});
 	const isAllValid = allValid.reduce((acc, current) => {
+	  // проверяем, чтобы все было правильно
 	  return acc && current;
 	});
-  
 	if (!Boolean(Number(isAllValid))) {
-	  e.preventDefault();
+	  alert("Заполните поля правильно!"); // если не правильно - сообщение пользователю
+	  return;
 	}
+	formSubmit(); // если правильно - отправляем данные
+  }
+
+  async function formSubmit() {
+	const data = serializeForm(form); // получаем данные формы
+	const response = await sendData(data); // отправляем данные на почту
+	if (response.ok) {
+	  let result = await response.json(); // если ответ OK отвечает пользователю
+	  alert(result.message); // .. что данные отправлены
+	  formReset(); // сбрасываем поля формы
+	} else {
+	  alert("Код ошибки: " + response.status); // если not OK - показываем код ошибки
+	}
+  }
+  
+  function serializeForm(form) {
+	// формируем данные формы
+	return new FormData(form);
+  }
+  
+  async function sendData(data) {
+	return await fetch("send_mail.php", {
+	  // отправляем в скрипт send_mail.php
+	  method: "POST", // методом POST
+	  body: data,
+	});
+  }
+  
+  function formReset() {
+	// сброс полей формы
+	form.reset();
+	validFormArr.forEach((el) => {
+	  el.setAttribute("is-valid", 0);
+	  el.style.border = "none";
+	});
   }
