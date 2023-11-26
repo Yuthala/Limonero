7cb275bdcb4de823307050f8c162ce67750f8742
Dina Polousova <dinapolousova@gmail.com>
2023-11-26 09:55:35 +0300

    styles form policy

--- js/form.js
+++ js/form.js
@@ -10,29 +10,41 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
 	
 	// отправка
-	let form = document.getElementById('form'); // переменная с формой
+	let form = document.getElementById('form'), // переменная с формой
 	// при отправке формы любым способом
-	form.addEventListener('submit', function (event) {
-		// запрещаем стандартное действие
-		event.preventDefault();
-		// создаем объект FormData
-		let data = new FormData(form);
-		console.log([data]);
-		// передаем в фукцию fetch данные и получаем результат
-		postData('send.php', data).then((data) => {
-			let result = document.getElementById('result');
-			// обработка ответа от сервера
-			if (data.error == '') {
-					// Очищаем поля формы и выводим пользователю сообщение обуспешной отправке
-					event.target.reset();
-					result.style.color = 'green';
-					result.textContent = "Ваше сообщение успешно отправлено";
-			} else {
-				result.style.color = 'red';
-				result.textContent = "При отправке формы произошла ошибка";
-			}
-		})
-	})
+		checkbox = document.getElementById('checkbox'); //переменная чекбокс для принятия политики конфиденциальности
+		
+
+	//проверка, что нажата галочка на политике конфиденциальности
+		if (checkbox.checked) {
+			form.addEventListener('submit', function (event) {
+				// запрещаем стандартное действие
+				event.preventDefault();
+				// создаем объект FormData
+				let data = new FormData(form);
+				console.log([data]);
+				// передаем в фукцию fetch данные и получаем результат
+				postData('send.php', data).then((data) => {
+					let result = document.getElementById('result');
+					// обработка ответа от сервера
+					if (data.error == '') {
+							// Очищаем поля формы и выводим пользователю сообщение обуспешной отправке
+							event.target.reset();
+							result.style.color = '#F7F6F6';
+							result.textContent = "Ваше сообщение успешно отправлено";
+					} else {
+						result.style.color = 'red';
+						result.textContent = "При отправке формы произошла ошибка";
+					}
+				})
+			});
+
+		} else {
+			result.style.color = 'red';
+			result.textContent = "Необходимо принять Политику конфиденциальности";
+		}
+
+
 	
 	});
 	
